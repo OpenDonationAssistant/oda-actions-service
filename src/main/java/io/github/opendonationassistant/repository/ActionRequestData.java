d640200fed05df909fddbec60a19fa5a17da3a2b
stCarolas <stcarolas@gmail.com>
2026-04-02 11:06:04 +0300

    fix saving actionrequest to db

--- src/main/java/io/github/opendonationassistant/repository/ActionRequestData.java
+++ src/main/java/io/github/opendonationassistant/repository/ActionRequestData.java
@@ -2,6 +2,8 @@ package io.github.opendonationassistant.repository;
 
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.MappedProperty;
+import io.micronaut.data.model.DataType;
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.annotation.Nullable;
 import java.util.List;
@@ -12,7 +14,7 @@ public record ActionRequestData(
   @Id String id,
   String source,
   @Nullable String originId,
-  List<ActionLink> actions
+  @MappedProperty(type = DataType.JSON) List<ActionLink> actions
 ) {
   @Serdeable
   public static record ActionLink(String id, String actionId, Integer amount) {}
